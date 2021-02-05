d3ece99e98ead9558ec51fff1ddcbf6d4acc7f0d
m3l1k4 <melikaihelas@gmail.com>
2021-02-04 21:03:37 -0800

    check render using expect statements

--- TodoReactSample/src/App.test.js
+++ TodoReactSample/src/App.test.js
@@ -1,24 +1,31 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {getQueriesForElement} from "@testing-library/dom";
+import { getQueriesForElement } from "@testing-library/dom";
+// import {render} from "@testing-library/react";
+import { App } from "./App";
 
-import {App} from "./App";
 
-test (" renders the correct content", () => {
-// redner a react component to the DOM
-  const root = document.createElement("div");
-  ReactDOM.render(<App />, root); 
+const render = () => {
+    const root = document.createElement("div");
+    ReactDOM.render(<App />, root);
+    return getQueriesForElement(root);
 
-const {getByText, getByLabelText} = getQueriesForElement(root);
-expect (getByText("TODOS")).not.toBeNull();
-expect (getByLabelText("What needs to be done?")).not.toBeNull();
-expect (getbyText(" Add #1")).not.toBeNull();
+}
 
-// //  Use DOm APIs ( queryselector) to make assertions
 
-//   expect ( root.querySelector("h1").textContent).toBe("TODOS")
-//   expect (root.querySelector("label").textContent).toBe("What needs to be done?");
-//   expect ( root.querySelector("button").textContent).toBe("Add #1")
+test(" renders the correct content", () => {
+    // render a react component to the DOM
+    const { getByText, getByLabelText } = render(<App />);
+
+    expect(getByText("TODOS")).not.toBeNull();
+    expect(getByLabelText("What needs to be done?")).not.toBeNull();
+    expect(getByText("Add #1")).not.toBeNull();
+
+    // //  Use DOm APIs ( queryselector) to make assertions
+
+    //   expect ( root.querySelector("h1").textContent).toBe("TODOS")
+    //   expect (root.querySelector("label").textContent).toBe("What needs to be done?");
+    //   expect ( root.querySelector("button").textContent).toBe("Add #1")
 
 
 });
