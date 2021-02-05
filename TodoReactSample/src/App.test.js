9c6b5074f55fa9112590093dc325011db9b9de04
m3l1k4 <melikaihelas@gmail.com>
2021-02-04 20:56:47 -0800

    Update App.test.js

--- TodoReactSample/src/App.test.js
+++ TodoReactSample/src/App.test.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {getQueriesForElement} from "@testing-library/dom";
 
 import {App} from "./App";
 
@@ -8,9 +9,16 @@ test (" renders the correct content", () => {
   const root = document.createElement("div");
   ReactDOM.render(<App />, root); 
 
-  // Use DOm APIs ( queryselector) to make assertions
+const {getByText, getByLabelText} = getQueriesForElement(root);
+expect (getByText("TODOS")).not.toBeNull();
+expect (getByLabelText("What needs to be done?")).not.toBeNull();
+expect (getbyText(" Add #1")).not.toBeNull();
 
-  expect ( root.querySelector("h1").textContent).toBe("TODOS")
-  expect (root.querySelector("label").textContent).toBe("What needs to be done?");
+// //  Use DOm APIs ( queryselector) to make assertions
+
+//   expect ( root.querySelector("h1").textContent).toBe("TODOS")
+//   expect (root.querySelector("label").textContent).toBe("What needs to be done?");
+//   expect ( root.querySelector("button").textContent).toBe("Add #1")
+
 
 });
